94f891238b7fcbfb9c46132a3c181f717b3dd6d5
vanakenm <martin.vanaken@8thcolor.com>
2014-12-21 15:16:08 +0100

    Content

--- js/main.js
+++ js/main.js
@@ -98,7 +98,6 @@ $('.chart').waypoint(function() {
 				{ src:'img/slider/01.jpg', fade:1000 },
 				{ src:'img/slider/02.jpg', fade:1000 },
 				{ src:'img/slider/03.jpg', fade:1000 },
-				{ src:'img/slider/04.jpg', fade:1000 }
 			  ]
 			})('overlay', {
 			  src:'img/overlays/16.png'
